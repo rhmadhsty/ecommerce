d2c5a143eaf3c672616079fe24cc5430223dc3ef
Alexaldi <95987508+Alexaldi@users.noreply.github.com>
2022-05-19 10:25:40 +0700

    Add files via upload

---
+++ ecommerce 0'.sql
@@ -0,0 +1,446 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 19 Bulan Mei 2022 pada 05.24
+-- Versi server: 10.1.31-MariaDB
+-- Versi PHP: 7.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ecommerce`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int(11) NOT NULL,
+  `adminname` varchar(50) NOT NULL,
+  `adminpass` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `bankwire`
+--
+
+CREATE TABLE `bankwire` (
+  `id_bank` int(11) NOT NULL,
+  `nama_bank` varchar(100) NOT NULL,
+  `logo_bank` varchar(150) NOT NULL,
+  `nama_akun` varchar(150) NOT NULL,
+  `no_rekening` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `foto`
+--
+
+CREATE TABLE `foto` (
+  `id_foto` int(11) NOT NULL,
+  `file_foto` varchar(200) NOT NULL,
+  `keterangan_foto` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `galery`
+--
+
+CREATE TABLE `galery` (
+  `id_galery` int(11) NOT NULL,
+  `foto` int(11) NOT NULL,
+  `vidio` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `informasi`
+--
+
+CREATE TABLE `informasi` (
+  `id_informasi` int(11) NOT NULL,
+  `nama_menu` varchar(25) NOT NULL,
+  `body_informasi` varchar(500) NOT NULL,
+  `posisi_informasi` int(11) NOT NULL,
+  `status_informasi` enum('Aktif','Tidak_Aktif','','') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `kategori` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `keranjang`
+--
+
+CREATE TABLE `keranjang` (
+  `id_keranjang` int(11) NOT NULL,
+  `id_pelanggan` int(11) NOT NULL,
+  `id_produk` int(11) NOT NULL,
+  `jumlah` int(5) NOT NULL,
+  `total_harga` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kontak`
+--
+
+CREATE TABLE `kontak` (
+  `id_kontak` int(11) NOT NULL,
+  `alamat_kontak` int(225) NOT NULL,
+  `no_hp_admin` int(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `order`
+--
+
+CREATE TABLE `order` (
+  `id_order` int(11) NOT NULL,
+  `id_pelanggan` int(11) NOT NULL,
+  `id_produk` int(11) NOT NULL,
+  `id_keranjang` int(11) NOT NULL,
+  `nama_pelanggan` varchar(70) NOT NULL,
+  `total_harga` int(20) NOT NULL,
+  `ongkir` varchar(20) NOT NULL,
+  `status_pengiriman` int(50) NOT NULL,
+  `jenis_ekpedisi` varchar(50) NOT NULL,
+  `alamat_penerima` varchar(225) NOT NULL,
+  `tanggal_order` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pelanggan`
+--
+
+CREATE TABLE `pelanggan` (
+  `id_pelanggan` int(11) NOT NULL,
+  `username` varchar(125) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `nohp` int(15) NOT NULL,
+  `password` varchar(225) NOT NULL,
+  `alamat` varchar(225) NOT NULL,
+  `kode_pos` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pembayaran`
+--
+
+CREATE TABLE `pembayaran` (
+  `id_pembayaran` int(11) NOT NULL,
+  `nama_pentransfer` varchar(100) NOT NULL,
+  `no_rekening` int(20) NOT NULL,
+  `nama_bank` varchar(150) NOT NULL,
+  `status_pembayaran` enum('done','undone','','') NOT NULL,
+  `bukti_pembayaran` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `produk`
+--
+
+CREATE TABLE `produk` (
+  `id_produk` int(11) NOT NULL,
+  `nama_produk` varchar(50) NOT NULL,
+  `foto_produk` varchar(50) NOT NULL,
+  `harga` int(20) NOT NULL,
+  `deskripsi_produk` varchar(225) NOT NULL,
+  `stok` int(10) NOT NULL,
+  `warna` varchar(10) NOT NULL,
+  `ukuran` varchar(3) NOT NULL,
+  `promo` int(10) NOT NULL,
+  `kategori` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `slideshow`
+--
+
+CREATE TABLE `slideshow` (
+  `id_slideshow` int(11) NOT NULL,
+  `foto_slideshow` varchar(225) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `sosmed`
+--
+
+CREATE TABLE `sosmed` (
+  `id_sosmed` int(11) NOT NULL,
+  `nama_sosmed` varchar(50) NOT NULL,
+  `logo_sosmed` varchar(100) NOT NULL,
+  `link_sosmed` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `vidio`
+--
+
+CREATE TABLE `vidio` (
+  `id_vidio` int(11) NOT NULL,
+  `link_vidio` varchar(225) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`),
+  ADD UNIQUE KEY `adminname` (`adminname`);
+
+--
+-- Indeks untuk tabel `bankwire`
+--
+ALTER TABLE `bankwire`
+  ADD PRIMARY KEY (`id_bank`),
+  ADD UNIQUE KEY `no_rekening` (`no_rekening`);
+
+--
+-- Indeks untuk tabel `foto`
+--
+ALTER TABLE `foto`
+  ADD PRIMARY KEY (`id_foto`);
+
+--
+-- Indeks untuk tabel `galery`
+--
+ALTER TABLE `galery`
+  ADD PRIMARY KEY (`id_galery`),
+  ADD KEY `foto` (`foto`),
+  ADD KEY `vidio` (`vidio`);
+
+--
+-- Indeks untuk tabel `informasi`
+--
+ALTER TABLE `informasi`
+  ADD PRIMARY KEY (`id_informasi`);
+
+--
+-- Indeks untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`),
+  ADD UNIQUE KEY `kategori` (`kategori`);
+
+--
+-- Indeks untuk tabel `keranjang`
+--
+ALTER TABLE `keranjang`
+  ADD PRIMARY KEY (`id_keranjang`);
+
+--
+-- Indeks untuk tabel `kontak`
+--
+ALTER TABLE `kontak`
+  ADD PRIMARY KEY (`id_kontak`);
+
+--
+-- Indeks untuk tabel `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`id_order`);
+
+--
+-- Indeks untuk tabel `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  ADD PRIMARY KEY (`id_pelanggan`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indeks untuk tabel `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`id_pembayaran`);
+
+--
+-- Indeks untuk tabel `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`id_produk`),
+  ADD KEY `kategori` (`kategori`);
+
+--
+-- Indeks untuk tabel `slideshow`
+--
+ALTER TABLE `slideshow`
+  ADD PRIMARY KEY (`id_slideshow`);
+
+--
+-- Indeks untuk tabel `sosmed`
+--
+ALTER TABLE `sosmed`
+  ADD PRIMARY KEY (`id_sosmed`);
+
+--
+-- Indeks untuk tabel `vidio`
+--
+ALTER TABLE `vidio`
+  ADD PRIMARY KEY (`id_vidio`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `bankwire`
+--
+ALTER TABLE `bankwire`
+  MODIFY `id_bank` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `foto`
+--
+ALTER TABLE `foto`
+  MODIFY `id_foto` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `galery`
+--
+ALTER TABLE `galery`
+  MODIFY `id_galery` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `informasi`
+--
+ALTER TABLE `informasi`
+  MODIFY `id_informasi` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `keranjang`
+--
+ALTER TABLE `keranjang`
+  MODIFY `id_keranjang` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `kontak`
+--
+ALTER TABLE `kontak`
+  MODIFY `id_kontak` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `order`
+--
+ALTER TABLE `order`
+  MODIFY `id_order` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  MODIFY `id_pembayaran` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `slideshow`
+--
+ALTER TABLE `slideshow`
+  MODIFY `id_slideshow` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `sosmed`
+--
+ALTER TABLE `sosmed`
+  MODIFY `id_sosmed` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `vidio`
+--
+ALTER TABLE `vidio`
+  MODIFY `id_vidio` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `galery`
+--
+ALTER TABLE `galery`
+  ADD CONSTRAINT `galery_ibfk_1` FOREIGN KEY (`foto`) REFERENCES `foto` (`id_foto`),
+  ADD CONSTRAINT `galery_ibfk_2` FOREIGN KEY (`vidio`) REFERENCES `vidio` (`id_vidio`);
+
+--
+-- Ketidakleluasaan untuk tabel `produk`
+--
+ALTER TABLE `produk`
+  ADD CONSTRAINT `produk_ibfk_1` FOREIGN KEY (`kategori`) REFERENCES `kategori` (`id_kategori`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
